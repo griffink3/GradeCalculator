3cbd8fe9e8bbc0bb9a80a3a14e4aee8b6d784ccb
Griffin Kao <griffinkao@administorsMBP3.home>
2018-06-21 21:30:36 -0400

    a

---
+++ FaceDate/FaceDate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDate
+//
+//  Created by Griffin Kao on 6/21/18.
+//  Copyright © 2018 Griffin Kao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
